19b532ef644b32f5438c23213d140293ff37d3ee
Mike Lowen <mike@mlowen.com>
2014-12-07 15:28:18 +1300

    Tied preview panel to editor

--- commonmark.editor.js
+++ commonmark.editor.js
@@ -6,13 +6,28 @@ define(['jquery', 'commonmark'], function ($, commonmark) {
 
 	$.fn.commonMarkEditor = function(options) {
 		this.each(function(index, item) {
+			var text = '';
+
 			// Preview
 			var preview = $('<div class="preview"></div>');
 
 			// The editor component
-			var textarea = $('<textarea class="form-control"></textarea>');
-			var editor = $('<div class="editor"></div>')
-				.append(textarea);
+			var onChange = function() {
+				var newValue = $(this).val();
+
+				if(newValue === text)
+					return;
+
+				text = newValue;
+				preview.html(convert(text));
+			};
+
+			var textarea = $('<textarea class="form-control"></textarea>')
+				.change(onChange)
+				.keydown(onChange)
+				.keyup(onChange);
+
+			var editor = $('<div class="editor"></div>').append(textarea);
 
 			$(item).addClass('commonmark-editor')
 				.append(editor)
