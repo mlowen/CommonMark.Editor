08a33724bea2847ae0a9d61d57bd8e07bcb5a2e7
Mike Lowen <mike@mlowen.com>
2014-12-14 09:39:54 +1300

    Updated Gruntfile
    Added less compilation.
    Fixed the banner so it didn't have an extra comment close.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,36 @@
 module.exports = function(grunt) {
+	var banner = '/*! <%= pkg.name %>-<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n';
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %>-<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n*/',
+				banner: banner,
 			},
 			build: {
 				src: '<%= pkg.name %>.js',
 				dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js'
+			}
+		},
+		less: {
+			development: {
+				options: {
+					banner: banner,
+					compress: true,
+					yuicompress: true,
+					optimization: 2
+				},
+				files: {
+					'dist/<%= pkg.name %>-<%= pkg.version %>.min.css': "commonmark.editor.less" // destination file and source file
+				}
 			}
 		}
 	});
 
 	/* Plugins */
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
 
 	/* Tasks */
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', [ 'uglify', 'less' ]);
 };
