547a1c97233fa0b2fd5d31d59145600886f2bc1c
Mike Lowen <mike@mlowen.com>
2014-12-23 21:27:50 +1300

    First cut of bindings
    The first cut of the bindings works only with simple types.

--- src/commonmark.editor.knockout.js
+++ src/commonmark.editor.knockout.js
@@ -1,6 +1,13 @@
 (function() {
 	var init = function($, ko) {
-		
+		if(ko.bindingHandlers.commonmarkEditor)
+			return;
+
+		ko.bindingHandlers.commonMarkEditor = {
+			init: function(element, accessor) {
+				var editor = $(element).commonMarkEditor({ text: accessor() })[0];
+			}
+		}
 	};
 
 	if(typeof define === 'function' && define.amd)
