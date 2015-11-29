a4de6e5d9f89c2eedf8f334ae17a7c57997be225
Mike Lowen <mlowen@lic.co.nz>
2015-11-29 15:44:15 +1300

    Updated exposition
    the library now exposes itself as an AMD plugin and binds itself to the
    global scope if the dependencies are available.

--- src/suffix.js
+++ src/suffix.js
@@ -1,7 +1,12 @@
 	};
 
-	if(typeof define === 'function' && define.amd)
+	var amdAvailable = typeof define === 'function' && define.amd; 
+
+	if(amdAvailable)
 		define([ 'jquery', 'commonmark' ], init);
-	else
+	
+	if(jQuery && commonmark)
 		init(jQuery, commonmark)
+	else if(!amdAvailable)
+		console.log('Unavble to load dependency for Bootstrap Commonmark Editor.');
 })();
